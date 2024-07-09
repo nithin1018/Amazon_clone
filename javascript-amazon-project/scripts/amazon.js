615218821a8433426ebd6d3405741fc20e14ff9a
Nithn <nithinjayakumar2205@gmail.com>
2024-07-09 22:45:27 +0530

    delete products from cart from array when clicking array

--- javascript-amazon-project/scripts/amazon.js
+++ javascript-amazon-project/scripts/amazon.js
@@ -17,7 +17,7 @@ products.forEach((product) =>{
 
           <div class="product-rating-container">
             <img class="product-rating-stars"
-              src="images/ratings/rating-${forproduct.rating.stars * 10}.png">
+              src="images/ratings/rating-${product.rating.stars * 10}.png">
             <div class="product-rating-count link-primary">
               ${product.rating.count}
             </div>
